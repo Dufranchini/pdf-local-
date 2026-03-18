68afec0f94eb2628a7d9bf4758d407a4eaa10acd
Carlos Eduardo <dufranchini@outlook.com>
2026-03-18 11:18:22 -0300

    tentativa de commit

--- main.py
+++ main.py
@@ -18,3 +18,7 @@ templates = Jinja2Templates(directory="templates")
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+
+
+#usea1-020.sentinelone.net
+
